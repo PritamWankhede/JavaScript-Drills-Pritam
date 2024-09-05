bc74e45d2ee6a91ab36ab8997244e042687044f6
PritamWankhede <pritamwankhede671@gmail.com>
2024-09-05 17:21:25 +0530

    Solution 3 uploaded

--- Drill2/Solution.js
+++ Drill2/Solution.js
@@ -1,10 +1,7 @@
 
 const {users} = require('../User.js');
 
-// const data = Object.keys(users);
-// console.log(data);
-// console.log(users);
-
+//Solution 1
 function solution1(users){
  let userName = [];
  let hobby = /video games/i;
@@ -19,4 +16,32 @@ return userName;
 }
 // console.log(solution1(users));
 
-module.exports = {solution1}; 
+//Solution 2
+function solution2(users){
+    let userInGermany = [];
+    for(let user in users){
+        if(users[user].nationality === "Germany"){
+            userInGermany.push(user);
+        }
+    }
+    return userInGermany;
+}
+// console.log(solution2());
+
+
+//Solution 3
+function solution3(users){
+    let masterDegreeUsers = [];
+      // console.log(users);
+    for(let user in users){
+      if(users[user].qualification === "Masters")
+       {
+          masterDegreeUsers.push(user);  
+       }
+    }
+  
+    return masterDegreeUsers.length !== 0 ? masterDegreeUsers.toString() : "No such user with Masters Degree";
+  }
+//   console.log(solution3(users));
+
+module.exports = {solution1,solution2,solution3}; 
--- Drill2/testCases.js
+++ Drill2/testCases.js
@@ -1,7 +1,17 @@
-const {solution1} = require('../Solutions/Solution1.js');
+const {solution1,solution2,solution3} = require('../Solutions/Solution1.js');
 const {users} = require('../User.js');
 
 //Solution 1 Testcases
 console.log(solution1(users));
 // console.log(solution1());
 // console.log(solution1([]));
+
+//Solution 2 Testcases
+console.log(solution2(users));
+// console.log(solution2());
+// console.log(solution2());
+
+//Solution 3 TestCases
+console.log(solution3(users));
+// console.log(solution3(3));
+//console.log(solution3());
