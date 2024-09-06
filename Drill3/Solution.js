7730f570018b0b1a2e68b383c2a9ccdf86dada7a
PritamWankhede <pritamwankhede671@gmail.com>
2024-09-06 10:39:19 +0530

    solution 2 uploaded

--- Drill3/Solution.js
+++ Drill3/Solution.js
@@ -2,15 +2,27 @@ const {inventory} =require('./InventoryData');
 
 // console.log(inventory);
 //Solution 1
-function solution1(inventory){
-    // console.log(inventory);
-  for(let index=0;index<inventory.length;index++)
-  {
-    if(inventory[index].id === 33)
-    {
-      console.log(`Car 33 is a ${inventory[index].car_year} ${inventory[index].car_make} ${inventory[index].car_model}`);
+// function solution1(inventory){
+//     // console.log(inventory);
+//   for(let index=0;index<inventory.length;index++)
+//   {
+//     if(inventory[index].id === 33)
+//     {
+//       console.log(`Car 33 is a ${inventory[index].car_year} ${inventory[index].car_make} ${inventory[index].car_model}`);
    
-    }
-  }
+//     }
+//   }
+// }
+// solution1(inventory);
+
+//Solution 2
+
+
+
+function solution2(inventory){
+    console.log([inventory.length]);
+    console.log(`Last car is a ${[inventory.length].car_make} ${[inventory.length].car_model}`);
 }
-solution1(inventory);
+// solution2(inventory);
+
+module.exports={solution2};
--- Drill3/testCases.js
+++ Drill3/testCases.js
@@ -1,7 +1,12 @@
-const {solution1} = require('./Solution');
+const {solution1,solution2} = require('./Solution');
 const {inventory} =require('./InventoryData');
+
 
 //Solution 1 testCase
 solution1(inventory);
 // solution1([]);
 // solution1();
+
+console.log(solution2(inventory));
+// console.log(solution2());
+// console.log(solution2([]));
