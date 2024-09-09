3aaa2aa5bdfb30d4da2af6f48e97e3756a28974d
PritamWankhede <pritamwankhede671@gmail.com>
2024-09-09 16:58:30 +0530

    Solution 2 of drill 5 added

--- Drill5/Objects.js
+++ Drill5/Objects.js
@@ -1,12 +1,23 @@
 const testObject = { name: "Bruce Wayne", age: 36, location: "Gotham" };
 
-function keys(obj) {
-let arr= [];
-  for(let key in obj)
-  {
-     arr.push(key);
-  }
-  return String(arr);
-}
-console.log(keys(testObject));
+// function keys(obj) {
+// let arr= [];
+//   for(let key in obj)
+//   {
+//      arr.push(key);
+//   }
+//   return String(arr);
+// }
+// console.log(keys(testObject));
+
+
+// function values(obj){
+//     let arr =[];
+//     for(let key in obj){
+//         arr.push(obj[key]);
+//     }
+//     return String(arr);
+// }
+// console.log(values(testObject));
+
 
