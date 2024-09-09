a964695e2b68c928c4a2835aa23e49a9a96f87b2
PritamWankhede <pritamwankhede671@gmail.com>
2024-09-09 10:05:59 +0530

    find methos uploaded

--- Drill4/Arrays.js
+++ Drill4/Arrays.js
@@ -1,16 +1,36 @@
 const items = [1, 2, 3, 4, 5, 5];
 
-function find(elements,cb){
+// function findEach(elements,cb){
   
-  for(let i=0;i<elements.length;i++)
+//     for(let i=0;i<elements.length;i++)
+//     {  
+    
+//        cb(elements[i],i);
+//     }
+//   }
+   
+//    function findNumber(element,i){
+//       console.log(`Element at ${i} is ${element}`)
+//    }
+  
+  
+//   console.log(find(items,findNumber));
+
+
+
+function find(elements,cb){
+   for(let i=0;i<elements.length;i++)
   {  
-     cb(elements[i],i);
+   
+    if(cb(elements[i])){
+        return true;
+    }
   }
+  return undefined;
 }
- 
- function findNumber(element,i){
-    console.log(`Element at ${i} is ${element}`)
+ function findNumber(element){
+  return element === 7;
  }
-
-
 console.log(find(items,findNumber));
+
+ 
